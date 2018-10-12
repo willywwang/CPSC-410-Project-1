d8496c14affaa84a0ee2abea58212c6b335442e2
Carol Leung <carol.leung@xerusmedical.com>
2018-10-11 22:15:18 -0700

    added some comments

--- src/ast/DISPLAY.java
+++ src/ast/DISPLAY.java
@@ -1,7 +1,12 @@
 package ast;
 
+import ui.Main;
+
+import javax.script.ScriptException;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DISPLAY extends STATEMENT {
     NAME name;
@@ -44,7 +49,22 @@ public class DISPLAY extends STATEMENT {
     }
 
     @Override
-    public String evaluate() throws FileNotFoundException, UnsupportedEncodingException {
+    public String evaluate() throws FileNotFoundException, UnsupportedEncodingException, ScriptException {
+        if(everyone){
+            //need to loop through each entry in symbol table and add anything that is a negative number
+        } else {
+            String n = name.evaluate();
+            HashMap<String, Float> d = Main.symbolTable.get(n);
+            //loop through each entry in
+            for (Map.Entry<String, Float> entry : d.entrySet()) {
+                System.out.println(entry.getKey() + " = " + entry.getValue());
+            }
+        }
+        if(debts){
+            //return dot program string a -> b thing
+        } else {
+            //return what it would look like as a transaction
+        }
         return null;
     }
 }
